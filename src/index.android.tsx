df584f10a301b022949ef2b08f6436e4bdd810f1
Adem Özay <ozayadem@gmail.com>
2017-06-07 14:35:38 +0300

    implemented onSave and onFailure methods in sample

--- src/index.android.tsx
+++ src/index.android.tsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { Toast } from 'native-base'
 
 import { Survey } from './survey/Survey'
 
 export default class App extends Component<{}, {}> {
 
-    public render() {
-        return (
-            <Survey form={require('../samples/form.json')} />
-        )
-    }
+	public render() {
+		return (
+			<Survey
+				form={require('../samples/form.json')}
+				onSave={this.onSave.bind(this)}
+				onFailure={this.onFailure.bind(this)} />
+		)
+	}
+
+	private onSave(answers: Object) {
+		Toast.show({
+			text: JSON.stringify(answers),
+			buttonText: 'Tamam',
+			position: 'bottom',
+			type: 'success',
+		})
+	}
+
+	private onFailure(erros: string[]) {
+		Toast.show({
+			text: erros.join('\n'),
+			buttonText: 'Tamam',
+			position: 'bottom',
+			type: 'danger',
+		})
+	}
 
 }
--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Toast } from 'native-base'
 
 import { Survey } from './survey/Survey'
 
@@ -6,8 +7,29 @@ export default class App extends Component<{}, {}> {
 
 	public render() {
 		return (
-			<Survey form={require('../samples/form.json')} />
+			<Survey
+				form={require('../samples/form.json')}
+				onSave={this.onSave.bind(this)}
+				onFailure={this.onFailure.bind(this)} />
 		)
+	}
+
+	private onSave(answers: Object) {
+		Toast.show({
+			text: JSON.stringify(answers),
+			buttonText: 'Tamam',
+			position: 'bottom',
+			type: 'success',
+		})
+	}
+
+	private onFailure(erros: string[]) {
+		Toast.show({
+			text: erros.join('\n'),
+			buttonText: 'Tamam',
+			position: 'bottom',
+			type: 'danger',
+		})
 	}
 
 }
