b1001e7e655dd38f06feb64d1e34b624b7ad73a5
Adem Özay <ozayadem@gmail.com>
2017-06-12 17:32:28 +0300

    bugfix. check options count in instance variable not in props

--- __tests__/ListInput.spec.tsx
+++ __tests__/ListInput.spec.tsx
@@ -5,18 +5,18 @@ import { ListInput } from '../src/components'
 describe('ListInput />', () => {
 
 	const options = {
-	type: 'static',
-	values: [
-		{
-			name: 'One',
-			value: '1',
-		},
-		{
-			name: 'Two',
-			value: '2',
-		},
-	],
-}
+		type: 'static',
+		values: [
+			{
+				name: 'One',
+				value: '1',
+			},
+			{
+				name: 'Two',
+				value: '2',
+			},
+		],
+	}
 
 	const wrapper = mount(
 		<ListInput tag="listinput" options={options} titleKey="name" valueKey="value" />,
@@ -28,8 +28,7 @@ describe('ListInput />', () => {
 		expect(component.props.tag).toBe('listinput')
 		expect(component.props.titleKey).toBe('name')
 		expect(component.props.valueKey).toBe('value')
-		expect(component.props.options.values.length).toBe(3) // options.length + list caption
-
+		expect(component.options.length).toBe(3) // options.length + list caption
 	})
 
 })
